1c7a9dc98218d7e4c320bfb6dae589d4de0f803a
MuhammadRezaHeidary <muhammadreza.heidary@gmail.com>
2023-12-15 22:26:09 +0330

    Rectangle Divider test

---
+++ Electron.py
@@ -0,0 +1,94 @@
+# Example
+
+# w = 10
+# h = 5
+# measurements on x-axis: 2, 5
+# measurements on y-axis: 3
+
+#    ___2______5__________ 
+#   |   |      |          |
+#   |   |      |          |
+#  3|___|______|__________|
+#   |   |      |          |
+#   |___|______|__________|
+
+# Number of squares in sub-rectangles = 4 (one 2x2, one 3x3, two 5x5)
+
+
+import sys
+import math
+
+w, h, count_x, count_y = [10, 5, 2, 3]
+measureX = [3, 6]
+measureY = [3, 6]
+
+def mergeDict(dict1, dict2):
+    dict3 = {**dict1, **dict2}
+    for key, value in dict3.items():
+        if key in dict1 and key in dict2:
+            dict3[key] = value + dict1[key]
+    
+    return dict3
+    
+def countUnique(cutsList):
+    #print("countUnique start with list ", list)
+    unique = set(cutsList)
+    result = {}
+    for i in unique:
+        result[i] = cutsList.count(i)
+    
+    return result
+    
+def getCuts(length, measures):
+    #print("getCuts start with length %s and measures %s" % (length, measures))
+    listLength = len(measures)
+    if listLength > 1:
+        tempList = [(measures[-1] - measures[i]) for i in range(listLength) if (measures[-1] - measures[i]) > 0]
+    else:
+        tempList = []
+    tempList.append(measures[-1])
+    cut = length - measures[-1]   
+    if cut > 0:
+        tempList.append(cut)
+    
+    return countUnique(tempList)
+    
+
+def getCutList(cutsList, length, measures):
+    #print("getCutsList2 start with cutsList %s, length %s and measures %s" % (cutsList, length, measures))
+    for i in range(1, len(measures)+1):
+        cutsList = mergeDict(cutsList, getCuts(length, measures[:i]))
+        
+    try:
+        cutsList[length] = 1
+    except:
+        cutsList[length] += 1
+    
+    return cutsList
+    
+    
+def countTotals(dictX, dictY):
+    #print("countTotals start with dictX %s and dictY %s" % (dictX, dictY))
+    result = 0
+    
+    if len(dictX) >= len(dictY):
+        smaller = dictY
+        bigger = dictX
+    else:
+        smaller = dictX
+        bigger = dictY
+    
+    for key in smaller:
+        try:
+            result += bigger[key] * smaller[key]
+        except:
+            pass
+             
+    return result
+
+
+cutsX = getCutList({}, w, measureX)
+cutsY = getCutList({}, h, measureY)
+
+
+print(countTotals(cutsX, cutsY))
